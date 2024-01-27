1bf003f5c0adf5740c6eab74042343e2d84ea0b5
RedTechX <jeremy.fangyc@gmail.com>
2024-01-27 14:18:46 +0800

    submit

--- main.py
+++ main.py
@@ -3,41 +3,59 @@
 Q1: 
 '''
 
-# TODO: Write your code here
+def greeting(name):
+    return f'Hello, {name}!'
 
+
+print(greeting('Alice'))
+print(greeting('bob'))
 ##################################################
 '''
 Q2:
 '''
 
-# TODO: Write your code here
+def greeting(name1, name2):
+    return f'Hello, {name1} and {name2}!'
 
+
+print(greeting('Alex', 'Bob'))
 ##################################################
 '''
 Q3:
 '''
 
-# TODO: Write your code here
 
+def rect_area(width, height):
+    return width * height
+
+
+rect_area(12.3, 45.6)
 ##################################################
 '''
 Q4:
 '''
 
-# TODO: Write your code here
+def weird_child_song():
+    print('''Bought me a cat and the cat pleased me,
+I fed my cat under yonder tree.
+Cat goes fiddle-i-fee.
 
-##################################################
-'''
-Q5:
-'''
+Bought me a hen and the hen pleased me,
+I fed my hen under yonder tree.
+Hen goes chimmy-chuck, chimmy-chuck,
+Cat goes fiddle-i-fee.
 
-# TODO: Write your code here
+Bought me a duck and the duck pleased me,
+I fed my duck under yonder tree.
+Duck goes quack, quack, Hen goes chimmy-chuck, chimmy-chuck,
+Cat goes fiddle-i-fee.
 
-##################################################
-'''
-Q6:
-'''
+Bought me a goose and the goose pleased me,
+I fed my goose under yonder tree.
+Goose goes hissy, hissy,
+Duck goes quack, quack,
+Hen goes chimmy-chuck, chimmy-chuck,
+Cat goes fiddle-i-fee.''')
 
-# TODO: Write your code here
-
+def weird_child_song()
 ##################################################
